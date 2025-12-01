2489a8f6d99c0d302fd3197c3ebee6fcd5adcafa
Eve <zhengyeve@gmail.com>
2025-12-01 15:31:19 -0800

    Basic view

--- app.js
+++ app.js
@@ -3,12 +3,39 @@ const dotenv = require('dotenv')
 dotenv.config();
 // const connectDb = require('./config/db.config')
 
-// /Users/eve.zheng/dev/nodemongo-crud/app.js
 const app = express();
 const port = process.env.NODE_LOCAL_PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
+
+
+// Register ejs as .html. If we did
+// not call this, we would need to
+// name our views foo.ejs instead
+// of foo.html. The __express method
+// is simply a function that engines
+// use to hook into the Express view
+// system by default, so if we want
+// to change "foo.ejs" to "foo.html"
+// we simply pass _any_ function, in this
+// case `ejs.__express`.
+
+app.engine('.html', require('ejs').__express);
+
+// Optional since express defaults to CWD/views
+var path = require('node:path');
+app.set('views', path.join(__dirname, 'views'));
+
+// Path to our public directory
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Without this you would need to
+// supply the extension to res.render()
+// ex: res.render('users.html').
+app.set('view engine', 'html');
+
 
 const db = require("./models");
 
@@ -26,11 +53,18 @@ db.mongoose
   });
 
 
-// Home page
+// Hello World
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+// page renders 
+app.get('/home', (req, res) => {
+    res.render('home', {
+        title: "Home Page"
+    });
+
+});
 
 require("./routes/user.routes")(app)
 
